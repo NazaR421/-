5172ed4840d486bd8638c6bd62b398e0c7db6afc
NazaR421 <131592318+NazaR421@users.noreply.github.com>
2024-01-22 14:01:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from PIL import Image
+from PIL import ImageFilter
+
 
 file_path = "pes.jpg"  # Update with the correct path
 
@@ -12,6 +14,15 @@ try:
         pes_gray = pic_original.convert("L")
         pes_gray.save("pes1.jpg")
         pes_gray.show()
+
+        pes_blured=pic_original.filter(ImageFilter.BLUR)
+        pes_blured.save("pes2.jpg")
+        pes_blured.show()
+
+        pes_up=pic_original.transpose(Image.ROTATE_180)
+        pes_up.save("pes3.jpg")
+        pes_up.show()
+
 
 except FileNotFoundError:
     print(f"Error: The file '{file_path}' does not exist.")
