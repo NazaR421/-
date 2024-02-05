08e90afc1c4cc705d8d4c14726307dfba1b6d579
NazaR421 <131592318+NazaR421@users.noreply.github.com>
2024-02-05 17:56:54 +0200

    Update easy_editor.py

--- easy_editor.py
+++ easy_editor.py
@@ -1,7 +1,11 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QVBoxLayout, \
+import os
+from PyQt5.QtWidgets import( QWidget, QPushButton, QLabel, QVBoxLayout, \
     QHBoxLayout, QApplication, QListWidget,QFileDialog
-import os                      
+)
+from Pil import ImageFilter,Image
+from PyQt5.QtGui import QPixmap                 
+
 
 app = QApplication([])
 
@@ -57,15 +61,32 @@ def showFilenamesList():
     global workdir
     extensions=[".jpg",".jpeg",".png",".gif"]
     choooseWorkdir()
+    files=os.listdir(workdir)
     filenames= filter(os.listdir(workdir),extensions)
     list_photo.clear()
     for filename in filenames:
         list_photo.addItem(filename)
-
-    
 btn_folder.clicked.connect(showFilenamesList)
 
 
+class ImageProcessor():
+    def __init__(self):
+        self.image=None
+        self.dir=None
+        self.filename=None
+        self.save_dir="Mified/"
+    def LoadImage(self,dir,filename):
+        self.dir=dir
+        self.filename=filename
+        image_path=os.path.join(dir,filename)
+        self.image = Image.open(image_path)
+    def showImage(self,path):
+        photo.hide()
+        pixmapimage=QPixmap(path)
+        w,h=photo.width(),photo.height()
+        pixmapimage=pixmapimage.scaled(w,h,Qt.KeepAspectRatio)
+        photo.setPixmap(pixmapimage)
+        photo.show()
 
 
 
