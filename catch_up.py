4959c2e01c8131f770ee08b60e5698217932fc56
NazaR421 <131592318+NazaR421@users.noreply.github.com>
2024-03-04 17:54:19 +0200

    Update catch_up.py

--- catch_up.py
+++ catch_up.py
@@ -5,14 +5,23 @@ window=display.set_mode((700,500))
 display.set_caption("Догонялки")
 background=transform.scale(image.load("background.jpg"),(700,500))
 
+mixer.init()
+mixer.music.load("jungles.ogg")
+mixer.music.play()
+
 x1=100
 y1=300
-x2=300
+x2=500
 y2=300
+x3=500
+y3=400
 
-sprite1 = transform.scale(image.load("sprite1.jpg"),(100,100))
-sprite2 = transform.scale(image.load("sprite2.jpg"),(100,100))
+
+sprite3 = transform.scale(image.load("treasure.png"),(70,70))
+sprite2 = transform.scale(image.load("cyborg.png"),(70,70))
+sprite1 = transform.scale(image.load("sprite1.jpg"),(50,50))
 speed=10
+
 
 
 run=True
@@ -23,6 +32,7 @@ while run:
     window.blit(background,(0,0))
     window.blit(sprite1,(x1,y1))
     window.blit(sprite2,(x2,y2))
+    window.blit(sprite3,(x3,y3))
 
     for e in event.get():
         if e.type ==QUIT:
@@ -32,22 +42,11 @@ while run:
     if keys_pressed[K_LEFT] and x1>5:
         x1 -=speed
     if keys_pressed[K_RIGHT] and x1<595:
-        x1 +=speed
+       x1 +=speed
     if keys_pressed[K_UP] and y1>5:
         y1 -=speed
     if keys_pressed[K_DOWN] and y1<395:
         y1 +=speed
 
-    if keys_pressed[K_a] and x2>5:
-        x2 -=speed
-    if keys_pressed[K_d] and x2<595:
-        x2 +=speed
-    if keys_pressed[K_w]and y2>5:
-        y2 -=speed
-    if keys_pressed[K_s] and y2<395:
-        y2 +=speed
-
-
     display.update()
-    clock.tick(FPS)
-
+    clock.tick(FPS) 
