2cb3763cfe260ec927738903d452a6d754a63129
NazaR421 <131592318+NazaR421@users.noreply.github.com>
2024-03-04 18:01:59 +0200

    Update catch_up.py

--- catch_up.py
+++ catch_up.py
@@ -48,5 +48,21 @@ while run:
     if keys_pressed[K_DOWN] and y1<395:
         y1 +=speed
 
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image,player_x,player_y,player_speed):
+        super().__init__()
+        self.image=transform.scale(image.load(player_image),(65,65))
+        self.speed=player_speed
+        self.rect=self.image.get_rect()
+        self.rect.x=player_x
+        self.rect.y=player_y
+
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+
+
+
+
+
     display.update()
     clock.tick(FPS) 
