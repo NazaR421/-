dac9816461c7d00c7d63157106826a27d2a53a52
NazaR421 <131592318+NazaR421@users.noreply.github.com>
2024-03-18 17:56:02 +0200

    Update catch_up.py

--- catch_up.py
+++ catch_up.py
@@ -1,68 +1,92 @@
-from pygame import*
-
-
-window=display.set_mode((700,500))
-display.set_caption("Догонялки")
-background=transform.scale(image.load("background.jpg"),(700,500))
-
-mixer.init()
-mixer.music.load("jungles.ogg")
-mixer.music.play()
-
-x1=100
-y1=300
-x2=500
-y2=300
-x3=500
-y3=400
-
-
-sprite3 = transform.scale(image.load("treasure.png"),(70,70))
-sprite2 = transform.scale(image.load("cyborg.png"),(70,70))
-sprite1 = transform.scale(image.load("sprite1.jpg"),(50,50))
-speed=10
-
-
-
-run=True
-clock=time.Clock()
-FPS=60
-
-while run:
-    window.blit(background,(0,0))
-    window.blit(sprite1,(x1,y1))
-    window.blit(sprite2,(x2,y2))
-    window.blit(sprite3,(x3,y3))
-
-    for e in event.get():
-        if e.type ==QUIT:
-            run=False
-
-    keys_pressed=key.get_pressed()
-    if keys_pressed[K_LEFT] and x1>5:
-        x1 -=speed
-    if keys_pressed[K_RIGHT] and x1<595:
-       x1 +=speed
-    if keys_pressed[K_UP] and y1>5:
-        y1 -=speed
-    if keys_pressed[K_DOWN] and y1<395:
-        y1 +=speed
-
+from pygame import *
+'''Необхідні класи'''
+ 
+#клас-батько для спрайтів
 class GameSprite(sprite.Sprite):
-    def __init__(self,player_image,player_x,player_y,player_speed):
+    #конструктор класу
+    def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
-        self.image=transform.scale(image.load(player_image),(65,65))
-        self.speed=player_speed
-        self.rect=self.image.get_rect()
-        self.rect.x=player_x
-        self.rect.y=player_y
+        #кожен спрайт повинен зберігати властивість image - зображення
+        self.image = transform.scale(image.load(player_image), (65, 65))
+        self.speed = player_speed
+        #кожен спрайт повинен зберігати властивість rect - прямокутник, в який він вписаний
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
 
     def reset(self):
-        window.blit(self.image,(self.rect.x,self.rect.y))
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
 
+# клас-спадкоємець для спрайта-ворога (переміщається сам)
+class Enemy(GameSprite):
+    direction = "left"
+
+
+    def update(self):
+        if self.rect.x <= 470:
+            self.direction = "right"
+        if self.rect.x >= win_width - 85:
+            self.direction = "left"
+
+
+        if self.direction == "left":
+            self.rect.x -= self.speed
+        else:
+            self.rect.x += self.speed 
+#Ігрова сцена:
+win_width = 700
+win_height = 500
+
+
+window = display.set_mode((win_width, win_height))
+display.set_caption("Maze")
+background = transform.scale(image.load("background.jpg"), (win_width, win_height))
+ 
+#Персонажі гри:
+player = Player('hero.png', 5, win_height - 80, 4)
+monster = Enemy('cyborg.png', win_width - 80, 280, 2)
+final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
+ 
+game = True
+clock = time.Clock()
+FPS = 60
+
+
+#музика
+mixer.init()
+mixer.music.load('jungles.ogg')
+mixer.music.play()
+
+finish=False
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if finish!=True:
+        window.blit(background,(0, 0))
+        player.update()
+        monster.update()
+
+        player.reset()
+        monster.reset()
 
 
 
     display.update()
-    clock.tick(FPS) 
+    clock.tick(FPS)
+
