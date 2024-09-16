29c462b9ab9a43a34a37ebdfc549b9218dc3263d
NazaR421 <131592318+NazaR421@users.noreply.github.com>
2024-09-16 15:50:11 +0300

    Update Advice.py

--- Advice.py
+++ Advice.py
@@ -41,6 +41,7 @@ class Car(Screen):
         self.add_widget(line)
         btn1.on_press=self.next
         btn2.on_press=self.next1
+        btn3.on_press=self.next7
     def next(self):
         self.manager.transition.direction="up"
         self.manager.current="car1"
@@ -48,6 +49,9 @@ class Car(Screen):
     def next1(self):
         self.manager.transition.direction="up"
         self.manager.current="car2"
+    def next7(self):
+        self.manager.transition.direction="up"
+        self.manager.current="car5"
     
 
 class Car1(Screen):
@@ -88,17 +92,17 @@ class Car2(Screen):
         
         lab_name = Label(text="Ви вибрали цінову категорію 10000$-50000$.Виберіть тип машини.", 
                          size_hint=(0.2,0.05), pos_hint={"y":0.5,"x":0.4})
-        sedan=Button(text="Седан",size_hint=(0.2,0.2),pos_hint={"x":0.07,"y":0.01})
-        krosover=Button(text="Кроссовер",size_hint=(0.2,0.2),pos_hint={"x":0.40,"y":0.01})
-        supercar=Button(text="Суперкар",size_hint=(0.2,0.2),pos_hint={"x":0.72,"y":0.01})
+        sedan=Button(text="Седан",size_hint=(0.2,0.2),pos_hint={"x":0.2,"y":0.01})
+        krosover=Button(text="Кроссовер",size_hint=(0.2,0.2),pos_hint={"x":0.6,"y":0.01})
+       
         line.add_widget(lab_name)
         line.add_widget(sedan)
         line.add_widget(krosover)
-        line.add_widget(supercar)
+
         self.add_widget(line)
         sedan.on_press=self.next2
         krosover.on_press=self.next3
-        supercar.on_press=self.next4
+
     def next2(self):
         self.manager.transition.direction="up"
         self.manager.current="car3"
@@ -135,7 +139,7 @@ class Car3(Screen):
         line.add_widget(return1)
         return1.on_press=self.next5
     def next5(self):
-        self.manager.transition.direction="Left"
+        self.manager.transition.direction="left"
         self.manager.current="one"
 
 class Car4(Screen):
@@ -171,36 +175,98 @@ class Car5(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         line=FloatLayout()
-        self.add_widget(line)
-        lab_name = Label(text="Ви вибрали суперкар, цінова категорія 10000$-50000$.Результат знизу.", 
-                                        size_hint=(0.2,0.05), pos_hint={"y":0.9,"x":0.4})
-        bmw = Label(text="1)BMW X5 2015 ціна 27.500$.Пробіг-256000тис.км.Паливо дизель 3.0\nКоробка автомат.Знаходиться в м.Чернівці.Номер продавця-(0505669106)", 
-                         size_hint=(0.2,0.05), pos_hint={"y":0.75,"x":0.24 })
-        Tesla = Label(text="2)Tesla Model X 2017 ціна 25.900$.Пробіг-121000тис.км.Паливо електрична 245кВт\nКоробка автомат.Знаходиться в м.Київ.Номер продавця-(0938218471)", 
-                         size_hint=(0.2,0.05), pos_hint={"y":0.63,"x":0.27})
-        audi = Label(text="3)Audi Q7 2018 ціна 35.000$.Пробіг-283000тисю.км.Паливо Дизель 3.0\nКоробка автомат. Знаходиться в Стрий,Львівська обл.Номер продавця-(0963649000)", 
-                         size_hint=(0.2,0.05), pos_hint={"y":0.48,"x":0.282})
-        Toyota = Label(text="4)Toyota Land Cruiser Prado 2010 ціна 25.000$.Пробіг-215000тис.км.Паливо дизель 3.0\nКоробка автомат. Знаходиться в м.Одеса.Номер продавця-(0955833367)", 
-                         size_hint=(0.2,0.05), pos_hint={"y":0.33,"x":0.281})
-        mazda = Label(text="5)Mazda CX-5 2020 ціна 21.399$.Пробіг 58000тис.км.Паливо бензин 2.5\nКоробка автомат.Знаходиться в м.Вінниця.Номер продавця-(0937314642)", 
-                         size_hint=(0.2,0.05), pos_hint={"y":0.23,"x":0.23})
         
+        lab_name = Label(text="Ви вибрали цінову категорію 10000$-50000$.Виберіть тип машини.", 
+                         size_hint=(0.2,0.05), pos_hint={"y":0.5,"x":0.4})
+        sedan1=Button(text="Седан",size_hint=(0.2,0.2),pos_hint={"x":0.2,"y":0.01})
+        krosover1=Button(text="Кроссовер",size_hint=(0.2,0.2),pos_hint={"x":0.6,"y":0.01})
+       
+        line.add_widget(lab_name)
+        line.add_widget(sedan1)
+        line.add_widget(krosover1)
+        self.add_widget(line)
+        sedan1.on_press=self.next6
+        krosover1.on_press=self.next8
+    def next6(self):
+        self.manager.transition.direction="right"
+        self.manager.current="car6"
+    def next8(self):
+        self.manager.transition.direction="left"
+        self.manager.current="car7"
+
+class Car6(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        line=FloatLayout()
+        self.add_widget(line)
+        lab_name = Label(text="Ви вибрали кросовери, цінова категорія 50000$-100000$.Результат знизу.", 
+                                        size_hint=(0.2,0.05), pos_hint={"y":0.9,"x":0.4})
+        bmw = Label(text="1)BMW 5 Series 2021 ціна 60.000$.Пробіг-33000тис.км.Паливо бензин 3.0\nКоробка автомат.Знаходиться в м.Київ.Номер продавця-(0664440600) ", 
+                         size_hint=(0.2,0.05), pos_hint={"y":0.75,"x":0.23 })
+        mercedes = Label(text="2)Mercedes-Benz E-Class 2022 ціна 65.000$.Пробіг-8000тис.км.Паливо гібрид 2.0\nКоробка автомат.Знаходиться в м.Київ.Номер продавця-(0678811555)", 
+                         size_hint=(0.2,0.05), pos_hint={"y":0.63,"x":0.261})
+        bmw2 = Label(text="3)BMW M3 2021 ціна 82.500$.Пробіг-32000тис.км.Паливо бензин 3.0\nКоробка автомат.Знаходиться в м.Київ.Номер продавця(0634814138)", 
+                         size_hint=(0.2,0.05), pos_hint={"y":0.48,"x":0.211})
+        Tycan = Label(text="4)Porsche Taycan 2022 ціна 78.000$тис.км.Електро.Коробка автомат.Знаходиться в м.Київ\nНомер продавця-(0960300013)", 
+                         size_hint=(0.2,0.05), pos_hint={"y":0.33,"x":0.3})
+        audi = Label(text="5)Audi S6 3.0 TDI Tiptronic Quattro S-Line 2024 ціна 94.000$.Нова\nПаливо дизель 3.0.Знаходиться в м.Одеса.Номер продавця(0632784598)", 
+                         size_hint=(0.2,0.05), pos_hint={"y":0.23,"x":0.23})
+        return1=Button(text="Назад",size_hint=(0.3,0.2),pos_hint={"x":0.3,"y":0.01})
+        line.add_widget(lab_name)
+        line.add_widget(bmw)
+        line.add_widget(mercedes)
+        line.add_widget(Tycan)
+        line.add_widget(bmw2)
+        line.add_widget(audi)
+        line.add_widget(return1)
+        return1.on_press=self.next9
+    def next9(self):
+        self.manager.transition.direction="down"
+        self.manager.current="one"
+
+
+class Car7(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        line=FloatLayout()
+        self.add_widget(line)
+        lab_name = Label(text="Ви вибрали кросовери, цінова категорія 50000$-100000$.Результат знизу.", 
+                                        size_hint=(0.2,0.05), pos_hint={"y":0.9,"x":0.4})
+        bmw = Label(text="1)BMW X5 2019 ціна 79.900$.Пробіг-118000тис.км.Паливо дизель 3.0\nКоробка автомат.Знаходиться в м.Одеса.Номер продавця-(0950040409)", 
+                         size_hint=(0.2,0.05), pos_hint={"y":0.75,"x":0.22 })
+        mercedes = Label(text="2)Mercedes-Benz G-Class 2012 ціна 71.000$.Пробіг-90000тис.км.Паливо бензин 5.5\nКоробка автомат.Знаходиться в м.Дніпро.Номер продавця(0973333080)", 
+                         size_hint=(0.2,0.05), pos_hint={"y":0.63,"x":0.261})
+        volkswagen = Label(text="3)Volkswagen Touareg 2018 ціна 56.000$.Пробіг-60000тис.км.Паливо дизель 3.0\nКоробка автомат.Знаходитьсяв м.Одеса.Номер продавця-(0950040409)", 
+                         size_hint=(0.2,0.05), pos_hint={"y":0.48,"x":0.25})
+        Tycan = Label(text="4)Toyota Land Cruiser 2017 ціна 61.500.Пробіг-82000тис.км.Паливо дизель 4.5\nКоробка автомат.Знаходиться м.Київ.Номер продавця-(0681970707)", 
+                         size_hint=(0.2,0.05), pos_hint={"y":0.33,"x":0.24})
+        audi = Label(text="5)Audi Q8 2020 ціна 75.000$.Пробіг-53000тис.км.Паливо дизель 3.0\nКоробка автомат.Знаходиться в м.Луцьк.Номер продавця(0506026605)", 
+                         size_hint=(0.2,0.05), pos_hint={"y":0.23,"x":0.223})
+        return1=Button(text="Назад",size_hint=(0.3,0.2),pos_hint={"x":0.3,"y":0.01})
+        line.add_widget(lab_name)
+        line.add_widget(bmw)
+        line.add_widget(mercedes)
+        line.add_widget(Tycan)
+        line.add_widget(volkswagen)
+        line.add_widget(audi)
+        line.add_widget(return1)
+        return1.on_press=self.next11
+    def next11(self):
+        self.manager.transition.direction="down"
+        self.manager.current="one"
 class MyApp(App):
     def build(self):
         main_screen= ScreenManager()
         main_screen.add_widget(One(name='one'))
         main_screen.add_widget(Car(name='car'))
-        main_screen.add_widget(Car1(name="car1"))
+        main_screen.add_widget(Car1(name="car1")) 
         main_screen.add_widget(Car2(name="car2"))
         main_screen.add_widget(Car3(name="car3"))
         main_screen.add_widget(Car4(name="car4"))
         main_screen.add_widget(Car5(name="car5"))
+        main_screen.add_widget(Car6(name="car6"))
+        main_screen.add_widget(Car7(name="car7"))
         return main_screen
-    
-
-
-app = MyApp()
-app.run()
     
 
 
