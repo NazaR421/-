30daee03c045049002b2bbc1447cdc872242b8ac
NazaR421 <131592318+NazaR421@users.noreply.github.com>
2024-11-04 20:01:17 +0200

    Update hero.py

--- m.n/hero.py
+++ m.n/hero.py
@@ -1,18 +1,20 @@
-key_switch_camera='c'
-key_switch_mode='z'
+key_switch_camera = 'c' # камера прив'язана до героя чи ні
+key_switch_mode = 'z' # можна проходити крізь перешкоди чи ні
 
-key_forward='w'
-key_back='s'
-key_right='a'
-key_left="d"
-key_up="e"
-key_down='q'
 
-key_turn_left='n'
-key_turn_right='m'
+key_forward = 'w'   # крок вперед (куди дивиться камера)
+key_back = 's'      # крок назад
+key_left = 'a'      # крок вліво (вбік від камери)
+key_right = 'd'     # крок вправо
+key_up = 'e'      # крок вгору
+key_down = 'q'     #крок вниз
+
+
+key_turn_left = 'n'     # поворот камери праворуч (а світу - ліворуч)
+key_turn_right = 'm'    # поворот камери ліворуч (а світу – праворуч)
 class Hero():
     def __init__(self,pos,land):
-        self.mode=True
+        self.mode = True
         self.land = land
         self.hero = loader.loadModel('smiley')
         self.hero.setColor(1, 0.5, 0)
@@ -43,112 +45,137 @@ class Hero():
 
     def accept_events(self):
         base.accept('c', self.changeView)
-        base.accept('n',self.turn_left)
-        base.accept('n'+'-repeat',self.turn_left)
-        base.accept('m',self.turn_right)
-        base.accept('m'+'-repeat',self.turn_right)
-        base.accept(key_up,self.up)
-        base.accept(key_up+'-repeat',self.up)
-        base.accept(key_down,self.down)
-        base.accept(key_down+'-repeat',self.down)
-        base.accept(key_left,self.left)
-        base.accept(key_left+"-repeat",self.left)
-        base.accept(key_right,self.right)
-        base.accept(key_right+"-repeat",self.right)
-        base.accept(key_forward,self.forward)
-        base.accept(key_forward+"-repeat",self.forward)
-        base.accept(key_back,self.back)
-        base.accept(key_back+"-repeat",self.back)
-        base.accept(key_switch_mode,self.changeMode)
-        base.accept(key_switch_mode+"-repeat",self.changeMode)
-        base.accept("b",self.build)
-        base.accept("v",self.destroy)
+        base.accept('n', self.turn_left)
+        base.accept('n'+'-repeat', self.turn_left)
+        base.accept('m', self.turn_right)
+        base.accept('m'+'-repeat', self.turn_right)
+
+        base.accept(key_up, self.up)
+        base.accept(key_up+'-repeat', self.up)
+        base.accept(key_down, self.down)
+        base.accept(key_down+'-repeat', self.down)
+
+        base.accept(key_left, self.left)
+        base.accept(key_left+'-repeat', self.left)
+        base.accept(key_right, self.right)
+        base.accept(key_right+'-repeat', self.right)
+        base.accept(key_forward, self.forward)
+        base.accept(key_forward+'-repeat', self.forward)
+        base.accept(key_back, self.back)
+        base.accept(key_back+'-repeat', self.back)
+
+        base.accept(key_switch_mode, self.changeMode)
+        base.accept(key_switch_mode+'-repeat', self.changeMode)
+
+        base.accept("b", self.build)
+        base.accept("v", self.destroy)
+       
+
+
 
 
     def turn_left(self):
-        self.hero.setH((self.hero.getH()+5)%360)
+        self.hero.setH((self.hero.getH() + 5) % 360)
     def turn_right(self):
-        self.hero.setH((self.hero.getH()-5)%360)
-    def look_at(self,angle):
-        x_from=round(self.hero.getX())
-        y_from=round(self.hero.getY())
-        Z_from=round(self.hero.getZ())
+        self.hero.setH((self.hero.getH() - 5) % 360)
 
-        dx,dy=self.check_dir(angle)
-        x_to=x_from+dx
-        y_to=y_from+dy
-        return x_to,y_to,z_from
-    def just_move(self,angle):
-        pos=self.look_at(angle)
-        self.hero.setPos(pos) 
-    def move_to(self,angle):
+    def look_at(self, angle):
+       x_from = round(self.hero.getX())
+       y_from = round(self.hero.getY())
+       z_from = round(self.hero.getZ())
+
+       dx, dy = self.check_dir(angle)
+       x_to = x_from + dx
+       y_to = y_from + dy
+       return x_to, y_to, z_from
+    
+    def just_move(self, angle):
+       pos = self.look_at(angle)
+       self.hero.setPos(pos)
+
+    def move_to(self, angle):
         if self.mode:
             self.just_move(angle)
         else:
             self.try_move(angle)
-    def try_move(self,angle):
-        pos=self.look_at(angle)
-        if self.land.isEmpty(pos):
-            pos=self.land.findHighestEmpty(pos)
-            self.hero.setPos(pos)
-        else:
-            pos=pos[0],pos[1],pos[2]+1
-            if self.lan.isEmpty(pos):
-                self.hero.setPos(pos)
+
+    def try_move(self, angle):
+       pos = self.look_at(angle)
+       if self.land.isEmpty(pos):
+           # маємо вільно. Можливо, треба впасти вниз:
+           pos = self.land.findHighestEmpty(pos)
+           self.hero.setPos(pos)
+       else:
+           # маємо зайнято. Якщо вийде, заберемося на цей блок:
+           pos = pos[0], pos[1], pos[2] + 1
+           if self.land.isEmpty(pos):
+               self.hero.setPos(pos)
+    
     def up(self):
-        if self.mode:
-            self.hero.setZ(self.hero.getZ()+1)
+       if self.mode:
+           self.hero.setZ(self.hero.getZ() + 1)
+
     def down(self):
-        if self.mode and self.hero.getZ()>1:
-            self.hero.setZ(self.hero.getZ()-1)
+       if self.mode and self.hero.getZ() > 1:
+           self.hero.setZ(self.hero.getZ() - 1)
+
     def changeMode(self):
-        if self.mode:
-            self.mode=False
-        else:
-            self.mode=True
+       if self.mode:
+           self.mode = False
+       else:
+           self.mode = True
+
     def forward(self):
-        angle=(self.hero.getH())% 360
-        self.move.move(angle)
+        angle =(self.hero.getH()) % 360
+        self.move_to(angle)
+
     def back(self):
-        angle=(self.hero.getH()+180)% 360
+        angle = (self.hero.getH()+180) % 360
         self.move_to(angle)
+    
     def left(self):
-        angle=(self.hero.getH()+90)% 360
+        angle = (self.hero.getH() + 90) % 360
         self.move_to(angle)
+
     def right(self):
-        angle=(self.hero.getH()+270)% 360
+        angle = (self.hero.getH() + 270) % 360
         self.move_to(angle)
+
+
+
     def check_dir(self,angle):
-        if angle>=0 and angle<=20:
-            return(0,-1)
-        elif angle <=65:
-            return(1,-1)
-        elif angle <=110:
-            return(1,0)
-        elif angle <=155:
-            return(1,1)
-        elif angle <=200:
-            return(0,1)
-        elif angle <=245:
-            return(-1,1)
-        elif angle <=290:
-            return(-1,0)
-        elif angle <=335:
-            return(-1,-1)
-        else:
-            return(0,1)
+       if angle >= 0 and angle <= 20:
+           return (0, -1)
+       elif angle <= 65:
+           return (1, -1)
+       elif angle <= 110:
+           return (1, 0)
+       elif angle <= 155:
+           return (1, 1)
+       elif angle <= 200:
+           return (0, 1)
+       elif angle <= 245:
+           return (-1, 1)
+       elif angle <= 290:
+           return (-1, 0)
+       elif angle <= 335:
+           return (-1, -1)
+       else:
+           return (0, -1)
+       
     def build(self):
-        angle=self.hero.getH()% 360
-        pos=self.look_at(angle)
+        angle = self.hero.getH() % 360
+        pos = self.look_at(angle)
         if self.mode:
             self.land.addBlock(pos)
         else:
             self.land.buildBlock(pos)
+
+
     def destroy(self):
-        angle=self.hero.getH()% 360
-        pos=self.loock_at(angle)
+        angle = self.hero.getH() % 360
+        pos = self.look_at(angle)
         if self.mode:
             self.land.delBlock(pos)
         else:
             self.land.delBlockFrom(pos)
-
